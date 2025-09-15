9f087aa8ff4ec3c76c25bfb3de99bbe3aedb7b74
Yakin Dev <yakin@example.com>
2025-09-15 20:34:19 +0200

    new commit

--- client-side/src/assets/assets.js
+++ client-side/src/assets/assets.js
@@ -14,29 +14,29 @@ import send_button from './send_button.svg'
 import menu_icon from './menu_icon.png'
 import arrow_icon from './arrow_icon.png'
 import code from './code.svg'
-import bgImage from './bgImage.svg'
 import pic1 from './pic1.png'
 import pic2 from './pic2.png'
+import bgImage from "/bgImage.svg";
 import pic3 from './pic3.png'
 import pic4 from './pic4.png'
 import img1 from './img1.jpg'
 import img2 from './img2.jpg'
 
 const assets = {
-    avatar_icon,
-    gallery_icon,
-    help_icon,
-    logo_big,
-    logo_icon,
-    logo,
-    search_icon,
-    send_button,
-    menu_icon,
-    arrow_icon,
-    code,
-    bgImage,
-    profile_martin
-}
+  avatar_icon,
+  gallery_icon,
+  help_icon,
+  logo_big,
+  logo_icon,
+  logo,
+  search_icon,
+  send_button,
+  menu_icon,
+  arrow_icon,
+  code,
+  bgImage,
+  profile_martin,
+};
 
 export default assets;
 
